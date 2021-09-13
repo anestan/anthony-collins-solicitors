e5b0c1c847fa2e5a20b91b33157cd512b076f7b0
Michael Ignacio <mikelignacio@gmail.com>
2021-09-13 14:59:27 +0800

    Update navigation menu

--- wp-content/themes/acs/functions.php
+++ wp-content/themes/acs/functions.php
@@ -216,3 +216,19 @@ function acs_acf_init_block_types() {
         ));
     }
 }
+
+/**
+ * Register Custom Navigation Walker
+ */
+function register_navwalker(){
+    require_once get_template_directory() . '/class-wp-bootstrap-navwalker.php';
+}
+add_action( 'after_setup_theme', 'register_navwalker' );
+
+/**
+ * Register Menu
+ */
+function register_my_menu() {
+	register_nav_menu('header-menu',__( 'Header Menu' ));
+}
+add_action( 'init', 'register_my_menu' );

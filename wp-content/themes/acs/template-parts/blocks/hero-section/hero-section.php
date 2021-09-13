2087e5f697a716fe034eeb4e7736d08d81c57e01
Michael Ignacio <mikelignacio@gmail.com>
2021-09-13 21:57:00 +0800

    Update hero section styles

--- wp-content/themes/acs/template-parts/blocks/hero-section/hero-section.php
+++ wp-content/themes/acs/template-parts/blocks/hero-section/hero-section.php
@@ -16,7 +16,7 @@ if( !empty($block['anchor']) ) {
 }
 
 // Create class attribute allowing for custom "className" and "align" values.
-$className = 'hero-section';
+$className = 'hero-section pt-5';
 if( !empty($block['className']) ) {
     $className .= ' ' . $block['className'];
 }
